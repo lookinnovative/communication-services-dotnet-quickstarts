6029d0d26673f486fb6deebf5445ff43a7296868
Anthony Robinson <anthony.robinson@lookinnovative.com>
2023-01-16 11:29:30 -0500

    turn-back

--- your-ui-lib-application/src/index.tsx
+++ your-ui-lib-application/src/index.tsx
@@ -1,16 +1,16 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
-import { App } from './App.snippet'; // this should point to your App.tsx file
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <div className="wrapper">
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
     <App />
-  </div>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
