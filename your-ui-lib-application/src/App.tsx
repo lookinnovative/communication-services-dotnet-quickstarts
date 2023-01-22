431f55811d0c975a4e3ca0806c631ddcc94d987a
Anthony Robinson <anthony.robinson@lookinnovative.com>
2023-01-22 08:25:46 -0500

    React has already been declared 147

--- your-ui-lib-application/src/App.tsx
+++ your-ui-lib-application/src/App.tsx
@@ -144,7 +144,7 @@ import {
   useAzureCommunicationCallAdapter,
   useAzureCommunicationChatAdapter
 } from '@azure/communication-react';
-import React, { CSSProperties, useEffect, useMemo, useRef, useState } from 'react';
+import React, { CSSProperties, useEffect, useMemo, useRef, useState };
 import { v4 as uuidv4 } from 'uuid';
 
 /**
