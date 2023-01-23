5e882700dca9f4ae67fcfda68d39efdc152280bc
Anthony Robinson <anthony.robinson@lookinnovative.com>
2023-01-23 07:24:13 -0500

    Import-Set Up Adapters

--- your-ui-lib-application/src/App.tsx
+++ your-ui-lib-application/src/App.tsx
@@ -1,4 +1,12 @@
-import React from 'react';
+import { ChatClient } from '@azure/communication-chat';
+import { AzureCommunicationTokenCredential, CommunicationUserIdentifier } from '@azure/communication-common';
+import {
+  fromFlatCommunicationIdentifier,
+  useAzureCommunicationCallAdapter,
+  useAzureCommunicationChatAdapter
+} from '@azure/communication-react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import logo from './logo.svg';
 import './App.css';
 
