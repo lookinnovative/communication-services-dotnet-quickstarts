53f7563ea492239f0f81e4f2e383ec62de36a138
Anthony Robinson <anthony.robinson@lookinnovative.com>
2023-01-24 08:31:33 -0500

    Set Up Adapters - complete

--- your-ui-lib-application/src/App.tsx
+++ your-ui-lib-application/src/App.tsx
@@ -14,25 +14,87 @@ const USER_ID = '<Azure Communication Services Identifier>';
 const TOKEN = '<Azure Communication Services Access Token>';
 const DISPLAY_NAME = '<Display Name>';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function App(): JSX.Element {
+  const { endpointUrl, UserId, tokem, displayName, groupId, threadId } = userAzureCommunicationServiceArgs();
+  const credential = useMemo(() => {
+    try {
+      return new AzureCommunicationTokenCredential(token);
+    } catch {
+      console.error('Failed to construct token credential');
+      return undefined;
+    }
+  }, [token]);
+  const callAdapterArgs = useMemo(
+    () => ({
+      userId: fromFlatCommunicationIdentifier(userId) as CommunicationUserIdentifier,
+      displayName,
+      credential,
+      locator: { groupId }
+    }),
+    [userId, credential, displayName, groupId]
   );
+  const callAdapter = useAzureCommunicationCallAdapter(callAdapterArgs);
+  const chatAdapterArgs = useMemo(
+    () => ({
+      endpoint: endpointUrl,
+      userId: fromFlatCommunicationIdentifier(userId) as CommunicationUserIdentifier,
+      displayName,
+      credential,
+      threadId
+    }),
+    [endpointUrl, userId, displayName, credential, threadId]
+  );
+  const chatAdapter = useAzureCommunicationChatAdapter(chatAdapterArgs);
+
+  if (!!callAdapter && !!chatAdapter) {
+    return <h1>Hooray! You set up adapters 🎉🎉🎉</h1>;
+  }
+  if (credential === undefined) {
+    return <h3>Failed to construct credential. Provided token is malformed.</h3>;
+  }
+  return <h3>Initializing...</h3>;
+}
+function useAzureCommunicationServiceArgs(): {
+  endpointUrl: string;
+  userId: string;
+  token: string;
+  displayName: string;
+  groupId: string;
+  threadId: string;
+} {
+  const [threadId, setThreadId] = useState('');
+  // For the quickstart, create a new thread with just the local participant in it.
+  useEffect(() => {
+    (async () => {
+      const client = new ChatClient(ENDPOINT_URL, new AzureCommunicationTokenCredential(TOKEN));
+      const { chatThread } = await client.createChatThread(
+        {
+          topic: 'Composites Quickstarts'
+        },
+        {
+          participants: [
+            {
+              id: fromFlatCommunicationIdentifier(USER_ID),
+              displayName: DISPLAY_NAME
+            }
+          ]
+        }
+      );
+      setThreadId(chatThread?.id ?? '');
+    })();
+  }, []);
+
+  return (
+    const groupId = useRef(uuidv4());
+
+  return {
+    endpointUrl: ENDPOINT_URL,
+    userId: USER_ID,
+    token: TOKEN,
+    displayName: DISPLAY_NAME,
+    groupId: groupId.current,
+    threadId
+  };
 }
 
 export default App;
