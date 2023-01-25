9aac5c4edbd80ce883dc69f6ce6c9d9495b94fd8
Anthony Robinson <anthony.robinson@lookinnovative.com>
2023-01-24 20:37:06 -0500

    Composites

--- your-ui-lib-application/src/App.tsx
+++ your-ui-lib-application/src/App.tsx
@@ -1,12 +1,24 @@
-import { ChatClient } from '@azure/communication-chat';
-import { AzureCommunicationTokenCredential, CommunicationUserIdentifier } from '@azure/communication-common';
 import {
+  AzureCommunicationTokenCredential,
+  CommunicationUserIdentifier,
+} from '@azure/communication-common';
+import {
+  CallComposite,
+  ChatComposite,
   fromFlatCommunicationIdentifier,
   useAzureCommunicationCallAdapter,
-  useAzureCommunicationChatAdapter
+  useAzureCommunicationChatAdapter,
 } from '@azure/communication-react';
-import React, { useEffect, useMemo, useRef, useState } from 'react';
+import React, {
+  CSSProperties,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import { ChatClient } from '@azure/communication-chat';
+import { initializeIcons } from '@fluentui/react';
 
 /**
  * Authentication information needed for your client application to use
@@ -30,13 +42,16 @@ const TOKEN = '<Azure Communication Services Access Token>';
  */
 const DISPLAY_NAME = '<Display Name>';
 
+initializeIcons();
+
 /**
  * Entry point of your application.
  */
 function App(): JSX.Element {
   // Arguments that would usually be provided by your backend service or
   // (indirectly) by the user.
-  const { endpointUrl, userId, token, displayName, groupId, threadId } = useAzureCommunicationServiceArgs();
+  const { endpointUrl, userId, token, displayName, groupId, threadId } =
+    useAzureCommunicationServiceArgs();
 
   // A well-formed token is required to initialize the chat and calling adapters.
   const credential = useMemo(() => {
@@ -52,10 +67,14 @@ function App(): JSX.Element {
   // a new adapter is only created when an argument changes.
   const callAdapterArgs = useMemo(
     () => ({
-      userId: fromFlatCommunicationIdentifier(userId) as CommunicationUserIdentifier,
+      userId: fromFlatCommunicationIdentifier(
+        userId
+      ) as CommunicationUserIdentifier,
       displayName,
       credential,
-      locator: { groupId }
+      locator: {
+        groupId,
+      },
     }),
     [userId, credential, displayName, groupId]
   );
@@ -66,24 +85,42 @@ function App(): JSX.Element {
   const chatAdapterArgs = useMemo(
     () => ({
       endpoint: endpointUrl,
-      userId: fromFlatCommunicationIdentifier(userId) as CommunicationUserIdentifier,
+      userId: fromFlatCommunicationIdentifier(
+        userId
+      ) as CommunicationUserIdentifier,
       displayName,
       credential,
-      threadId
+      threadId,
     }),
     [endpointUrl, userId, displayName, credential, threadId]
   );
   const chatAdapter = useAzureCommunicationChatAdapter(chatAdapterArgs);
 
   if (!!callAdapter && !!chatAdapter) {
-    return <h1>Hooray! You set up adapters 🎉🎉🎉</h1>;
+    return (
+      <div style={{ height: '100vh', display: 'flex' }}>
+        <div style={containerStyle}>
+          <ChatComposite adapter={chatAdapter} />
+        </div>
+        <div style={containerStyle}>
+          <CallComposite adapter={callAdapter} />
+        </div>
+      </div>
+    );
   }
   if (credential === undefined) {
-    return <h3>Failed to construct credential. Provided token is malformed.</h3>;
+    return (
+      <h3>Failed to construct credential. Provided token is malformed.</h3>
+    );
   }
   return <h3>Initializing...</h3>;
 }
 
+const containerStyle: CSSProperties = {
+  border: 'solid 0.125rem olive',
+  margin: '0.5rem',
+  width: '50vw',
+};
 /**
  * This hook returns all the arguments required to use the Azure Communication services
  * that would be provided by your backend service after user authentication
@@ -101,18 +138,21 @@ function useAzureCommunicationServiceArgs(): {
   // For the quickstart, create a new thread with just the local participant in it.
   useEffect(() => {
     (async () => {
-      const client = new ChatClient(ENDPOINT_URL, new AzureCommunicationTokenCredential(TOKEN));
+      const client = new ChatClient(
+        ENDPOINT_URL,
+        new AzureCommunicationTokenCredential(TOKEN)
+      );
       const { chatThread } = await client.createChatThread(
         {
-          topic: 'Composites Quickstarts'
+          topic: 'Composites Quickstarts',
         },
         {
           participants: [
             {
               id: fromFlatCommunicationIdentifier(USER_ID),
-              displayName: DISPLAY_NAME
-            }
-          ]
+              displayName: DISPLAY_NAME,
+            },
+          ],
         }
       );
       setThreadId(chatThread?.id ?? '');
@@ -129,7 +169,7 @@ function useAzureCommunicationServiceArgs(): {
     token: TOKEN,
     displayName: DISPLAY_NAME,
     groupId: groupId.current,
-    threadId
+    threadId,
   };
 }
 
